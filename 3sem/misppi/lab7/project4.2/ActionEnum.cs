5ab795ad5131571b8020aacfd547d6ea760aae3b
ZetoOfficial <tpptgs@bk.ru>
2022-11-14 16:55:34 +0500

    Вернул файлы, которые вижулка скрыла............

---
+++ 3sem/misppi/lab7/project4.2/ActionEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4._2
+{
+    public enum ActionEnum
+    {
+        Edit, New
+    }
+}
